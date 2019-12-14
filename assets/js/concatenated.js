aa3b7b23c214caa9d31b36e7a0d359eb1dde2e30
Blake (from Scroll If) <blake@scrollif.com.au>
2019-12-14 17:32:41 +1000

    file update (clear)
    .

---
+++ assets/js/concatenated.js
@@ -0,0 +1,7 @@
+---
+---
+{% remote_include https://blakedavies-assets.netlify.com/no-js/js/no-js.js %}
+{% remote_include https://blakedavies-assets.netlify.com/can-touch/js/can-touch.js %}
+{% remote_include https://blakedavies-assets.netlify.com/loading-screen/js/loading-screen.js %}
+{% remote_include https://blakedavies-assets.netlify.com/menu-icon/js/menu-icon.js %}
+{% remote_include https://blakedavies-assets.netlify.com/navbar-slider/js/navbar-slider.js %}
