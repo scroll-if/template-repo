7dd6e7f3bc1f1da0cb2d5c4ed508cf907ecd5862
Blake (from Scroll If) <blake@scrollif.com.au>
2023-03-12 20:46:32 +1100

    add

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "https://rubygems.org"
 gem 'jekyll'
+gem "jekyll-sass-converter", "~> 2.0"
 gem 'net-http'
 gem 'uri'
 gem 'nokogiri'
--- assets/js/concatenated.js
+++ assets/js/concatenated.js
@@ -3,6 +3,11 @@
 // GLOBAL JS //
 	{% remote_include https://scrollif-assets.netlify.app/websites/js/no-js.js %}
 	{% remote_include https://scrollif-assets.netlify.app/websites/js/can-touch.js %}
+	{% remote_include https://scrollif-assets.netlify.app/websites/js/remove-focus.js %}
+	{% remote_include https://scrollif-assets.netlify.app/websites/js/smooth-scroll.js %}
 	
 // EXTERNAL COMPONENTS //
 	{% remote_include https://scrollif-assets.netlify.app/websites/components/loading-screen/js/loading-screen.js %}
+
+// INTERNAL JS //
+	
